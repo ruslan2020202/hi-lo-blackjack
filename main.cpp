84e3c0bae6054af3df09fd3105ca8ba419b94279
Faer-faer <131977826+ruslan2020202@users.noreply.github.com>
2024-10-04 13:47:13 +0300

    add classes

--- main.cpp
+++ main.cpp
@@ -12,27 +12,35 @@ bool find_element(vector<char> list, char el){
     }
 }
 
-/* hi-lo */
-int count_card(char card){
-    const vector<char> high_cards {'k', 'd', 'v'};
-    const vector<char> mid_cards {'7', '8', '9'};
-    const vector<char> small_cards {'2', '3', '4', '5', '6'};
-    if (find_element(high_cards, card)){
-        return -1;
+class Tactics{
+    
+    public:
+        const vector<char> high_cards {'k', 'd', 'v'};
+        const vector<char> mid_cards {'7', '8', '9'};
+        const vector<char> small_cards {'2', '3', '4', '5', '6'};
+        
+    int count_card(char card){
+        
+        if (find_element(high_cards, card)){
+            return -1;
+        }
+        if(find_element(small_cards, card)){
+            return 1;
+        }
+        return 0;
     }
-    if(find_element(small_cards, card)){
-        return 1;
-    }
-    return 0;
-}
+};
+
+
 
 int main(){
     int count = 0;
     while (true){
         char card;
-        cout << "Enter your card";
+        Tactics hi_lo;
+        cout << "\nEnter your card";
         cin >> card;
-        count += count_card(card);
+        count += hi_lo.count_card(card);
         cout << count;
     }
     return 0;
